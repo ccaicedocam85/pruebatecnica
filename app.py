d7a4b437dbaff0af943e502ae61d3581d405bc4c
cristiancaicedoc <cristiancaicedoc@davivienda.com>
2024-02-22 15:07:21 -0500

    Disable CSRF protection in app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-# Disable CSRF protection
-app.config['WTF_CSRF_ENABLED'] = False
-
 # Initialize CSRF protection
 csrf = CSRFProtect(app)
 
